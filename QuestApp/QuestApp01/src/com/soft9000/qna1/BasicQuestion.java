0226dc77f96bee5d21237f23047e9b5163bad142
= <=>
2021-04-11 07:30:24 -0400

    Using a classic GNU JSON Library

---
+++ QuestApp/QuestApp01/src/com/soft9000/qna1/BasicQuestion.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.soft9000.qna1;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author profnagy
+ */
+public class BasicQuestion {
+    public int id = 0;
+    public String KID = "";
+    public String GID = "";
+    public String Difficulty = "";
+    public String Association = "";
+    public String Status = "";
+    public ArrayList<String> Question = new ArrayList<String>();
+    public ArrayList<String> Answer = new ArrayList<String>();
+}
---
+++ QuestApp/QuestApp01/src/com/soft9000/qna1/JsonOps.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.soft9000.qna1;
+
+import java.io.File;
+import java.util.List;
+
+
+/**
+ *
+ * @author profnagy
+ */
+public class JsonOps {
+    
+    public static List<BasicQuestion> ImportFile(File file) {
+        return null;
+    }
+    
+}
