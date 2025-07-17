10e06ba62dc03bc8ee683253839b233385e8f1d8
Mr. Nagy <r.a.nagy@gmail.com>
2025-07-17 05:41:46 -0400

    Update Adv04_DBM.py

--- FunctionMachines/Adv04_DBM.py
+++ FunctionMachines/Adv04_DBM.py
@@ -4,10 +4,13 @@
 # License: MIT
 # File: Adv04_DBM.py
 # Mission: Use S3D2 over the core DBM Package
-# Related:  https://github.com/Python3-Training/DatMan
-# Mentoring: https://tl.page/randall_nagy
+# Related: https://github.com/Python3-Training/DatMan
+# Source:  https://github.com/Python3-Training/DatMan/blob/master/FunctionMachines/Adv02.py
+# Short:   https://youtube.com/shorts/urRnkGywVEE?si=p5de5R-vNky65rCc
+# Video:   https://youtu.be/ZXcPPkn3JIc
+# Related: https://www.youtube.com/@TotalPythoneering
 # Author: Randall Nagy
-# Version: 1.0
+# Version: 1.0.b
 #endregion
 
 import os
