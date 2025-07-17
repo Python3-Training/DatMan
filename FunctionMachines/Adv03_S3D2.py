f565108952f854fa4ad31ea5e1f30e153daff226
Mr. Nagy <r.a.nagy@gmail.com>
2025-07-17 05:41:22 -0400

    Update Adv03_S3D2.py

--- FunctionMachines/Adv03_S3D2.py
+++ FunctionMachines/Adv03_S3D2.py
@@ -3,9 +3,12 @@
 # File: Adv03_S3D2.py
 # Mission: Implement a Source, Sync, Search, and Delete Pattern.
 # Related: https://github.com/Python3-Training/DatMan
-# Mentoring: https://tl.page/randall_nagy
+# Source:  https://github.com/Python3-Training/DatMan/blob/master/FunctionMachines/Adv02.py
+# Short:   https://youtube.com/shorts/urRnkGywVEE?si=p5de5R-vNky65rCc
+# Video:   https://youtu.be/ZXcPPkn3JIc
+# Related: https://www.youtube.com/@TotalPythoneering
 # Author: Randall Nagy
-# Version: 1.1.1
+# Version: 1.1.1.b
 import os
 import os.path
 import json
